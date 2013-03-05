01a0fb3b4a6e7b6410f357fd8a831d728eaf2e29
Heather Payne <heather@heatherpayne.ca>
2013-03-04 20:42:35 -0500

    Create new posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,18 @@ class PostsController < ApplicationController
 	def index
 		@posts = Post.all
 	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new(params[:post])
+		if @post.valid?
+			@post.save
+			redirect_to posts_path
+		else
+			render :new
+		end
+	end
 end
